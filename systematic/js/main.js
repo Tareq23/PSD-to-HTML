16d7b134aab03111bc9e078cecf1495316e69f70
Md Tarequl Islam <Tareq23@mdtarequlislam.github.com>
2021-01-15 21:16:49 +0600

    PSD to HTML

---
+++ systematic/js/main.js
@@ -0,0 +1,11 @@
+/*==========================
+BX SLIDER
+============================*/
+
+$(document).ready(function(){
+    $('.slider').bxSlider();
+    $('.main-menu').meanmenu({
+      meanMenuContainer : ".mobile-menu",
+      meanScreenWidth: 768,
+    }); 
+  });
